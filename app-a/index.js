7adeb8adfa1c2233fda749e9d872ab4503a7ce82
Noritaka IZUMI <noritaka.izumi@gmail.com>
2020-11-28 00:11:52 +0900

    修正

--- app-a/index.js
+++ app-a/index.js
@@ -4,6 +4,6 @@ const request = new XMLHttpRequest();
 request.open('GET', 'http://app-b/', true);
 request.responseType = 'json';
 request.addEventListener('load', () => {
-    document.getElementById('resp').innerHTML = request.response['SCRIPT_FILENAME'];
+    document.getElementById('resp').innerHTML = request.response['HTTP_USER_AGENT'];
 });
 request.send();
--- app-b/index.php
+++ app-b/index.php
@@ -2,7 +2,7 @@
 header('Content-type: text/json; charset=utf-8');
 $allowOrigin = 'http://site-c';
 header("Access-Control-Allow-Origin: ${allowOrigin}");
-$resp = array('SCRIPT_FILENAME' => $_SERVER['HTTP_USER_AGENT']);
+$resp = array('HTTP_USER_AGENT' => $_SERVER['HTTP_USER_AGENT']);
 sleep(2);
 echo json_encode($resp);
 exit;
